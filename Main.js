d7efb428b2f3df79633b3f536fe5c731310ba436
LukeIThink <ytluke07@gmail.com>
2021-04-12 01:25:24 -0500

    Made download website close in 5 seconds

--- Main.js
+++ Main.js
@@ -1,6 +1,6 @@
 async function download(link) {
   if (!link) { return; }
   const opened = await window.open(link, 'Download')
-  setTimeout(() => { opened.close(); }, 10);
+  setTimeout(() => { opened.close(); }, 5000);
 
 }
